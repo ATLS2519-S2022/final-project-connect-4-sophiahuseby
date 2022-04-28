05ab751d9cd3287fff23568226c6d2dbc7255715
sophiahuseby <89488269+sophiahuseby@users.noreply.github.com>
2022-04-28 15:47:31 -0600

    Add files via upload

---
+++ src/GreedyPlayer.java
@@ -0,0 +1,102 @@
+public class GreedyPlayer implements Player
+{
+   
+    int id; 
+    int cols; 
+    
+    @Override
+    public String name() {
+        return "Greedy Player";
+    }
+
+    @Override
+    public void init(int id, int msecPerMove, int rows, int cols) {
+    	this.id = id; //id is your player's id, opponent's id is 3-id
+    	this.cols = cols;
+    }
+
+    @Override
+    public void calcMove(
+        Connect4Board board, int oppMoveCol, Arbitrator arb) 
+        throws TimeUpException {
+        // Make sure there is room to make a move.
+        if (board.isFull()) {
+            throw new Error ("Complaint: The board is full!");
+        }
+        
+        int[] scoresArray = new int[cols];
+        
+        for (int i = 0; i < cols; i++) {
+        	if (board.isValidMove(i)) {
+        		board.move(i,  id);
+        		scoresArray[i] = calcScore(board, id) - calcScore(board, 3-id);
+        		board.unmove(i, id);
+        	}
+        	else {
+        		scoresArray[i] = -100;
+        	}
+        }
+        
+        int thisMove = 0;
+        
+        int tryOut = scoresArray[0];
+        
+        for (int i = 0; i < cols; i++) {
+        	if (scoresArray[i] > tryOut) {
+        		tryOut = scoresArray[i];
+        		thisMove = i;
+        	}
+        }
+        
+        arb.setMove(thisMove);
+    }
+    
+	// Return the number of connect-4s that player #id has.
+	public int calcScore(Connect4Board board, int id)
+	{
+		final int rows = board.numRows();
+		final int cols = board.numCols();
+		int score = 0;
+		// Look for horizontal connect-4s.
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c <= cols - 4; c++) {
+				if (board.get(r, c + 0) != id) continue;
+				if (board.get(r, c + 1) != id) continue;
+				if (board.get(r, c + 2) != id) continue;
+				if (board.get(r, c + 3) != id) continue;
+				score++;
+			}
+		}
+		// Look for vertical connect-4s.
+		for (int c = 0; c < cols; c++) {
+			for (int r = 0; r <= rows - 4; r++) {
+				if (board.get(r + 0, c) != id) continue;
+				if (board.get(r + 1, c) != id) continue;
+				if (board.get(r + 2, c) != id) continue;
+				if (board.get(r + 3, c) != id) continue;
+				score++;
+			}
+		}
+		// Look for diagonal connect-4s.
+		for (int c = 0; c <= cols - 4; c++) {
+			for (int r = 0; r <= rows - 4; r++) {
+				if (board.get(r + 0, c + 0) != id) continue;
+				if (board.get(r + 1, c + 1) != id) continue;
+				if (board.get(r + 2, c + 2) != id) continue;
+				if (board.get(r + 3, c + 3) != id) continue;
+				score++;
+			}
+		}
+		for (int c = 0; c <= cols - 4; c++) {
+			for (int r = rows - 1; r >= 4 - 1; r--) {
+				if (board.get(r - 0, c + 0) != id) continue;
+				if (board.get(r - 1, c + 1) != id) continue;
+				if (board.get(r - 2, c + 2) != id) continue;
+				if (board.get(r - 3, c + 3) != id) continue;
+				score++;
+			}
+		}
+		return score;
+	}
+
+}
